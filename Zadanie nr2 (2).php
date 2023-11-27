c79864aeab5cc9a1b6e8dcb757307112660d65a8
Dysmor <151465622+Dysmor@users.noreply.github.com>
2023-11-27 08:43:03 +0100

    Add files via upload

---
+++ Zadanie nr2 (2).php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie 2</title>
+    <style>
+body {
+    font-family: 'Arial', sans-serif;
+    background-color: #f4f4f4;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+
+
+.login-form {
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+label {
+    display: block;
+    margin-bottom: 5px;
+}
+
+input {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 10px;
+    box-sizing: border-box;
+}
+
+input[type="submit"] {
+    background-color: #4caf50;
+    color: white;
+    cursor: pointer;
+}
+
+input[type="submit"]:hover {
+    background-color: #45a049;
+}
+    </style>
+</head>
+<body>
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    if ($_POST["username"] == "admin" && $_POST["password"] == "test") {
+        echo "Logowanie udane!";
+    } else {
+        echo "Błąd logowania. Spróbuj ponownie.";
+    }
+}
+?>
+
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>" class="login-form">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required><br>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br>
+
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
